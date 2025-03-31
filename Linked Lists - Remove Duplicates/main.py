dd0517aa0fe332d657febb2f12e9a46101ecd561
yagodanr <yagodanr@gmail.com>
2025-03-31 20:33:00 +0300

    Initial Linked Lists - Remove Duplicates

---
+++ Linked Lists - Remove Duplicates/main.py
@@ -0,0 +1,9 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    # Your code goes here.
+    # Remember to return the head of the list.
+
