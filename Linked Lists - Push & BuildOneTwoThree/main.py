3f5e12bf209d35eff7082441e403c6ab3ca6b102
yagodanr <yagodanr@gmail.com>
2025-03-31 19:35:51 +0300

    Linked Lists - Push & BuildOneTwoThree

--- Linked Lists - Push & BuildOneTwoThree/main.py
+++ Linked Lists - Push & BuildOneTwoThree/main.py
@@ -3,11 +3,32 @@ class Node():
         self.data = data
         self.next = next
 
+    def __repr__(self):
+        def stringify(node: Node) -> str:
+            """
+            converts linked list to string
+            """
+            if node is None:
+                return "None"
+
+            return f"{node.data} -> {stringify(node.next)}"
+        return stringify(self)
+
 
 def push(head, data):
     # Your code goes here.
-    return Node(None)
+    if head is None:
+        return Node(data)
+
+    tmp = Node(data, head)
+    head = tmp
+
+    return head
 
 def build_one_two_three():
     # Your code goes here.
-    return Node(None)
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
