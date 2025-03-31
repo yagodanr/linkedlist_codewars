ff51740d776f0079c300d8e0e1b1ef0252601934
yagodanr <yagodanr@gmail.com>
2025-03-31 20:04:47 +0300

    Initial Linked Lists - Sorted Insert

---
+++ Linked Lists - Sorted Insert/main.py
@@ -0,0 +1,10 @@
+""" For your information:
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+"""
+
+def sorted_insert(head, data):
+    # Your code goes here.
+    # Make sure to return the head of the list.
