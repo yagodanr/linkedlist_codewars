40b8181e72f4fd16609705248dd661dd1eed1872
yagodanr <yagodanr@gmail.com>
2025-03-31 20:06:08 +0300

    Pulled Push & BuildOneTwoThree into Linked Lists - Sorted Insert

--- Linked Lists - Sorted Insert/main.py
+++ Linked Lists - Sorted Insert/main.py
@@ -1,10 +1,39 @@
-""" For your information:
-class Node(object):
-    def __init__(self, data):
+class Node():
+    def __init__(self, data, next = None):
         self.data = data
-        self.next = None
-"""
+        self.next = next
+
+    def __repr__(self):
+        def stringify(node: Node) -> str:
+            """
+            converts linked list to string
+            """
+            if node is None:
+                return "None"
+
+            return f"{node.data} -> {stringify(node.next)}"
+        return stringify(self)
+
+
+def push(head, data):
+    # Your code goes here.
+    if head is None:
+        return Node(data)
+
+    tmp = Node(data, head)
+    head = tmp
+
+    return head
+
+def build_one_two_three():
+    # Your code goes here.
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
+
 
 def sorted_insert(head, data):
-    # Your code goes here.
-    # Make sure to return the head of the list.
+    ...
+
