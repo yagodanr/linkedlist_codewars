a60de760a175d406de48d67fd6fb10b1ff797c34
yagodanr <yagodanr@gmail.com>
2025-03-31 20:45:46 +0300

    Linked Lists - Remove Duplicates

--- Linked Lists - Remove Duplicates/main.py
+++ Linked Lists - Remove Duplicates/main.py
@@ -6,4 +6,15 @@ class Node(object):
 def remove_duplicates(head):
     # Your code goes here.
     # Remember to return the head of the list.
+    if head is None:
+        return None
 
+    cur = head
+    while cur is not None:
+        if cur.next is None:
+            return head
+        if cur.data == cur.next.data:
+            cur.next = cur.next.next
+            continue
+        cur = cur.next
+    raise Exception
