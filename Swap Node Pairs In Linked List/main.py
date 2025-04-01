d0e766fcc8a8a48e5a7f6dcbdfa3c70e47d5e540
yagodanr <yagodanr@gmail.com>
2025-04-01 21:41:52 +0300

    Swap Node Pairs In Linked List

--- Swap Node Pairs In Linked List/main.py
+++ Swap Node Pairs In Linked List/main.py
@@ -1,20 +1,42 @@
 class Node:
+    IDS = 0
     def __init__(self, next=None):
         self.next = next
+        self.id = Node.IDS
+        Node.IDS += 1
+
+    def __repr__(self):
+        def stringify(node: Node) -> str:
+            """
+            converts linked list to string
+            """
+            if node is None:
+                return "None"
+
+            return f"{node.id} -> {stringify(node.next)}"
+        return stringify(self)
 
 def swap_pairs(head):
     if head is None or head.next is None:
         return head
 
     cur = head
-    next = cur.next
-
-    while next is not None:
-        cur.next, next.next = next.next, cur
-        cur = cur.next
-        if cur is None:
+    head = head.next
+    prev = None
+    while cur is not None:
+        try:
+            n2 = cur.next.next
+        except AttributeError:
             return head
-        next = cur.next
+        cur.next.next = cur
+        if prev is not None:
+            prev.next = cur.next
+        prev = cur
+        cur.next = n2
+        cur = cur.next
+
 
     return head
 
+print(swap_pairs(Node(Node(Node()))))
+print(swap_pairs(Node(Node(Node(Node())))))
