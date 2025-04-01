a86494baebace92060de43a563158fb27ef6c750
yagodanr <yagodanr@gmail.com>
2025-04-01 21:20:51 +0300

    Tried Swap Node Pairs In Linked List

--- Swap Node Pairs In Linked List/main.py
+++ Swap Node Pairs In Linked List/main.py
@@ -3,4 +3,18 @@ class Node:
         self.next = next
 
 def swap_pairs(head):
+    if head is None or head.next is None:
+        return head
+
+    cur = head
+    next = cur.next
+
+    while next is not None:
+        cur.next, next.next = next.next, cur
+        cur = cur.next
+        if cur is None:
+            return head
+        next = cur.next
+
     return head
+
